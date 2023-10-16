f87dc2dc1aa4d37d6d328439292315f5e5c9a745
lautroRocha <lautarovrocha@gmail.com>
2023-10-16 15:26:34 -0300

    styles: css styles

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,8 @@
 import { getBooks } from "./firebaseServices.js";
 
 const main = document.querySelector('main');
-const CART_COUNT = document.querySelector('#cart')
+const CART_COUNT = document.querySelector('#cart-count')
+const cartBtn = document.querySelector('#cart-btn')
 export const BOOKS = [];
 const CART = {selected: [], total: 0};
 const cartItems = document.querySelector('#cart-items')
@@ -11,21 +12,27 @@ const closeCartBtn = document.querySelector('#close-cart')
 
 
 export const drawCard = (obj) => {
-    const {name, release, cover, price} = obj
+    const {name, release, cover, author, price} = obj
     let bookCard = document.createElement('div')
     let bookTitle = document.createElement('h2')
+    let bookAuthor = document.createElement('span')
     let bookRelease = document.createElement('span')
+    let bookPrice = document.createElement('span')
     let bookCover = document.createElement('img')
     let addButton = document.createElement('button')
     bookCard.classList.add('book-card')
     bookTitle.textContent = name;
+    bookAuthor.textContent = author
     bookRelease.textContent = release
+    bookPrice.textContent = "$ " + price
     addButton.textContent = 'ADD TO CART'
     addButton.onclick = () => addToCart(obj)
     bookCover.setAttribute('src', cover)
     bookCard.appendChild(bookTitle)
+    bookCard.appendChild(bookAuthor)
     bookCard.appendChild(bookCover)
     bookCard.appendChild(bookRelease)
+    bookCard.appendChild(bookPrice)
     bookCard.appendChild(addButton)
     main.appendChild(bookCard)
 }
@@ -55,7 +62,7 @@ const updateTotalPrice = () => {
 }
 
 if(!window.location.pathname.includes('admin')) {
-    CART_COUNT.onclick = () => {
+    cartBtn.onclick = () => {
         aside.classList.toggle('closed')
     }
     closeCartBtn.onclick = () => {
